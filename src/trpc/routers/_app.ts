5a2a37f2a8f78c92cbd22d1d315d95373c32fd07
RISHII <93994298+RISHII7@users.noreply.github.com>
2026-06-27 20:20:49 +0530

    feat(executions): add trpc routers and params for execution queries
    - Created executionsRouter with getOne and getMany queries
    
    - Registered executionsRouter in app router
    
    - Added prefetch methods for execution queries
    
    - Configured nuqs search params and loader for pagination

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,13 @@
 import { createTRPCRouter } from "@/trpc/init";
 
+import { executionsRouter } from "@/features/executions/server/routers";
 import { workflowsRouter } from "@/features/workflows/servers/routers";
 import { credentialsRouter } from "@/features/credentials/server/routers";
 
 export const appRouter = createTRPCRouter({
   workflows: workflowsRouter,
   credentials: credentialsRouter,
+  executions: executionsRouter,
 });
 
 // export type definition of API
